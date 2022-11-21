45491470d03d465723dd95b3ddd1d3dae3b4c071
ifntsh <bizifntsh@gmail.com>
2022-11-21 17:40:30 +0900

    Initial commit

---
+++ src/main/java/group/loacon/LoaconApplication.java
@@ -0,0 +1,13 @@
+package group.loacon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoaconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoaconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group/loacon/LoaconApplicationTests.java
@@ -0,0 +1,13 @@
+package group.loacon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoaconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
